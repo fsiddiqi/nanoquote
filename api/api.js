5a926ce901a534e07bc42878b8f361c4469dde68
Faisal Siddiqi <FSiddiqi@FSiddiqi-Mac.local>
2016-04-06 12:25:59 -0400

    Added DELETE and PATCH.  PATCH incomplete

--- api/api.js
+++ api/api.js
@@ -19,13 +19,14 @@ exports.getApi = function (req, res) {
  */
 exports.postApiQuotes = function (req, res) {
     state.quotes = state.quotes || [];
-    //theBody = JSON.parse(req.body);
     theBody = req.body;
+    // Generate a unique quote_id
+    theBody.quote_id = _.uniqueId();
     // set response body and send
     state.quotes.push(theBody);
     return res.json({
         status: "ok",
-        state: state.quotes
+        state: theBody
     });
 };
 
@@ -40,7 +41,7 @@ exports.getApiQuotes = function (req, res) {
     // set response body and send
     res.json({
         status: "ok",
-        state: state.quotes
+        quotes: state.quotes
     });
 };
 
@@ -62,13 +63,78 @@ exports.getApiQuotes2 = function (req, res) {
     var quote = _.find(state.quotes, {
         'quote_id': quoteID
     });
-    //if(quote === undefined){
-    //    throw({message: "Can't find " + quoteID});
-    //}
+    if (quote === undefined) {
+        throw ({
+            message: "Can't find " + quoteID
+        });
+    }
 
     // set response body and send
     return res.json({
         status: "ok",
         state: quote
+    });
+};
+
+/*
+ * PATCH /api/quotes/{quote_id}
+ *
+ * Parameters (named path params accessible on req.params and query params on req.query):
+ *
+ * quote_id(type: integer) - path parameter - ID of quote to update
+ */
+exports.patchApiQuotes = function (req, res) {
+    req.check('quote_id', 'Invalid parameter').notEmpty();
+    if (req.validationErrors()) {
+        return res.json(400, req.validationErrorsJson());
+    }
+    // Find matching quote
+    var quoteID = req.params.quote_id;
+    var quote = _.find(state.quotes, {
+        'quote_id': quoteID
+    });
+    if (quote === undefined) {
+        res.json({
+            status: 404
+        });
+        //throw({message: "Can't find " + quoteID});
+    }
+    quote = req.body;
+    // set response body and send
+    // BUGGY - FIX
+    res.json({
+        status: "ok",
+        quote: quote
+    });
+};
+
+/*
+ * DELETE /api/quotes/{quote_id}
+ *
+ * Parameters (named path params accessible on req.params and query params on req.query):
+ *
+ * quote_id(type: integer) - path parameter - ID of quote to delete
+ */
+exports.deleteApiQuotes = function (req, res) {
+    req.check('quote_id', 'Invalid parameter').notEmpty();
+    if (req.validationErrors()) {
+        return res.json(400, req.validationErrorsJson());
+    }
+
+    // Find matching quote
+    var quoteID = req.params.quote_id;
+    var quote = _.find(state.quotes, {
+        'quote_id': quoteID
+    });
+    if (quote === undefined) {
+        res.json({
+            status: 404
+        });
+        //throw({message: "Can't find " + quoteID});
+    }
+    state.quotes = _.without(state.quotes, quote);
+    // set response body and send
+    res.json({
+        status: 200
     });
 };
