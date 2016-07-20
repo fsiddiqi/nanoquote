63db3d2eab76b36a87e141b239a55cf753c23e63
Faisal Siddiqi <FSiddiqi@FSiddiqi-Mac.local>
2016-07-20 11:57:42 -0400

    updates

--- api/api.js
+++ api/api.js
@@ -1,14 +1,16 @@
+
+
 /*
  * GET /api/
  *
  * Parameters (named path params accessible on req.params and query params on req.query):
  *
  */
-exports.getApi = function (req, res) {
-    res.status(200);
+exports.getApi = function(req, res) {
+	res.status(200);
 
-    // set response body and send
-    res.json({});
+	// set response body and send
+	res.json({});
 };
 
 /*
@@ -17,7 +19,7 @@ exports.getApi = function (req, res) {
  * Parameters (body params accessible on req.body for JSON, req.xmlDoc for XML):
  *
  */
-exports.postApiQuotes = function (req, res) {
+exports.postApiQuotes = function(req, res) {
     state.quotes = state.quotes || [];
     theBody = req.body;
     // Generate a unique quote_id
@@ -39,9 +41,9 @@ exports.postApiQuotes = function (req, res) {
  *
  * Parameters (named path params accessible on req.params and query params on req.query):
  *
- * category(type: string) - query parameter - quote category
+ * first_name(type: string) - query parameter - quote category
  */
-exports.getApiQuotes = function (req, res) {
+exports.getApiQuotes = function(req, res) {
     var quote;
     var firstName = req.query.first_name;
     var quoteDate = req.query.quote_date;
@@ -58,6 +60,9 @@ exports.getApiQuotes = function (req, res) {
         quote = _.filter(state.quotes, {
             'quote_date': quoteDate
         });
+        //if(quote === undefined){
+        //    throw({message: "Can't find " + firstName});
+        //}
         quotes = quote;
     }
     else
@@ -72,6 +77,7 @@ exports.getApiQuotes = function (req, res) {
         state: quotes
     });
 
+    //res.json(state.quotes);
 };
 
 /*
@@ -81,7 +87,7 @@ exports.getApiQuotes = function (req, res) {
  *
  * quote_id(type: integer) - path parameter - ID of quote to fetch
  */
-exports.getApiQuotes2 = function (req, res) {
+exports.getApiQuotes2 = function(req, res) {
     req.check('quote_id', 'Invalid parameter').notEmpty();
     if (req.validationErrors()) {
         return res.json(400, req.validationErrorsJson());
@@ -92,10 +98,8 @@ exports.getApiQuotes2 = function (req, res) {
     var quote = _.find(state.quotes, {
         'quote_id': quoteID
     });
-    if (quote === undefined) {
-        throw ({
-            message: "Can't find " + quoteID
-        });
+    if(quote === undefined){
+        throw({message: "Can't find " + quoteID});
     }
 
     // set response body and send
@@ -112,25 +116,24 @@ exports.getApiQuotes2 = function (req, res) {
  *
  * quote_id(type: integer) - path parameter - ID of quote to update
  */
-exports.patchApiQuotes = function (req, res) {
-    req.check('quote_id', 'Invalid parameter').notEmpty();
-    if (req.validationErrors()) {
-        return res.json(400, req.validationErrorsJson());
-    }
+exports.patchApiQuotes = function(req, res) {
+	req.check('quote_id', 'Invalid parameter').notEmpty();
+	if (req.validationErrors()) {
+		return res.json(400,req.validationErrorsJson());
+	}
     // Find matching quote
     var quoteID = req.params.quote_id;
-    var quote = _.find(state.quotes, {
-        'quote_id': quoteID
-    });
-    if (quote === undefined) {
-        res.json({
-            status: 404
-        });
+    var quote = _.find(state.quotes, {'quote_id': quoteID});
+    if(quote === undefined){
+    	res.json({status: 404});
         //throw({message: "Can't find " + quoteID});
     }
     // update the quote object
     _.merge(quote, req.body);
-    res.json({
+    //quote = req.body;
+	// set response body and send
+	// BUGGY - FIX
+	res.json({
         status: "ok",
         quote: quote
     });
@@ -143,26 +146,22 @@ exports.patchApiQuotes = function (req, res) {
  *
  * quote_id(type: integer) - path parameter - ID of quote to delete
  */
-exports.deleteApiQuotes = function (req, res) {
-    req.check('quote_id', 'Invalid parameter').notEmpty();
-    if (req.validationErrors()) {
-        return res.json(400, req.validationErrorsJson());
-    }
+exports.deleteApiQuotes = function(req, res) {
+	req.check('quote_id', 'Invalid parameter').notEmpty();
+	if (req.validationErrors()) {
+		return res.json(400,req.validationErrorsJson());
+	}
 
     // Find matching quote
     var quoteID = req.params.quote_id;
     var quote = _.find(state.quotes, {
         'quote_id': quoteID
     });
-    if (quote === undefined) {
-        res.json({
-            status: 404
-        });
+    if(quote === undefined){
+    	res.json({status: 404});
         //throw({message: "Can't find " + quoteID});
     }
     state.quotes = _.without(state.quotes, quote);
     // set response body and send
-    res.json({
-        status: 200
-    });
+	res.json({status: 200});
 };
